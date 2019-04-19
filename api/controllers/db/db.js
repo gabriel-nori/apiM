fdac0d96fac2fcc92e319b08dca5a601125614a6
Gabriel Nori <gabriel@zazcar.com.br>
2019-04-19 14:09:32 -0300

    Removed comment version control

--- api/controllers/db/db.js
+++ api/controllers/db/db.js
@@ -7,17 +7,3 @@ module.exports = mysql.createConnection({
   password: credential.password,
   database: credential.dbName
 });
-
-
-
-/*con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-});
-
-
-*/
